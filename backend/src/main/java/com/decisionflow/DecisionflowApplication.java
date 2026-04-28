dc5e3871d8d43dbcf5ca0e5253d5c458106bc405
KamilKonopski <Kamil.Konopski.2000@gmail.com>
2026-04-28 20:47:51 +0200

    chore: initialize backend project

---
+++ backend/src/main/java/com/decisionflow/DecisionflowApplication.java
@@ -0,0 +1,13 @@
+package com.decisionflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecisionflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecisionflowApplication.class, args);
+	}
+
+}
